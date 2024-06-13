fe092b979f020208ef66883b7c76ac7591083974
Shubham Jadhav <shubhamjadhav21022002@gmail.com>
2024-06-13 08:23:01 +0530

    added bubble sort code

--- arrays/NegativePositive.java
+++ arrays/NegativePositive.java
@@ -2,15 +2,29 @@
 
 package arrays;
 
+import java.util.Arrays;
+
 public class NegativePositive {
 
     public static void main(String[] args) {
         int[] arr = {1,-3,5,-7,3,-6,-9};
+
+        sortNegativePositive(arr);
+
+        System.out.println(Arrays.toString(arr));
     }
     
     static void sortNegativePositive(int arr[]){
 
-        
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
 
     }
 }
